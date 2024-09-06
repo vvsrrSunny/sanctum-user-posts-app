0f908a47777a2dbb7b62e55c39c58e109cb06466
vvsrrsunny <bharadwaj.varanasi96@gmail.com>
2024-09-06 21:47:32 +0800

    added post policy

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PostPolicy
+{
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Post $post): Response
+    {
+        return $user->id === $post->user_id ? Response::allow()
+            : Response::deny('You are not authorized to update this post');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Post $post): Response
+    {
+        return $user->id === $post->user_id ? Response::allow()
+            : Response::deny('You are not authorized to delete this post');
+    }
+}
